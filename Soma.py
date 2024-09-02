40196ca67598f9147c828d393514dc305f19b3b2
Joao Wictor <joaowictorpedroni@hotmail.com>
2024-09-02 15:24:16 -0300

    Tecnica_1

---
+++ Soma.py
@@ -0,0 +1,12 @@
+# Variáveis iniciais
+INDICE = 13
+SOMA = 0
+K = 0
+
+# Loop para calcular SOMA
+while K < INDICE:
+    K += 1
+    SOMA += K
+
+# Imprime o resultado
+print("O valor da variável SOMA é:", SOMA)
